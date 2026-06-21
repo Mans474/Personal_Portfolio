f6d5ccc7d499aca16d4d5c956d26f9e5b7399f94
Mansi Parekh <mansiparekh1303@gmail.com>
2026-06-21 18:34:26 +0530

    Enhance preloader script with scroll to top
    Added scroll to top functionality on load.

--- Personal Portfolio/script.js
+++ Personal Portfolio/script.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", () => {
     console.log("Preloader js working");
     const preloader = document.getElementById("preloader");
+    window.scrollTo(0,0);
     setTimeout(() => {
         preloader.classList.add("fade-out");
         setTimeout(() => {
